87518e6bb33ed9dc2440fddc2c3be988b733f399
nicholasraynes <nicholasraynes@yahoo.com>
2025-04-06 09:43:33 -0400

    feat: add generate pdf service

---
+++ app/services/pdf_generator.py
@@ -0,0 +1,6 @@
+import os
+from weasyprint import HTML
+
+def generate_pdf(content: bytes, output_path: str):
+    with open(output_path, "wb") as f:
+        HTML(string=content.decode("utf-8")).write_pdf(f)
