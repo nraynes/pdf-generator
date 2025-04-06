7e96faad077ffc62944b3c3a564b170aaf8a9941
nicholasraynes <nicholasraynes@yahoo.com>
2025-04-06 10:03:24 -0400

    feat: added tasks for creating pdf and getting status

---
+++ app/workers/tasks.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from app.services.pdf_generator import generate_pdf
+import os
+
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+celery = Celery("worker", broker=redis_url, backend=redis_url)
+
+@celery.task(name="create_pdf_task")
+def create_pdf_task(filename: str, content: bytes):
+    job_id = create_pdf_task.request.id
+    output_path = f"generated_pdfs/{job_id}.pdf"
+    os.makedirs("generated_pdfs", exist_ok=True)
+    generate_pdf(content, output_path)
+    return job_id
+
+def get_task_status(task_id: str):
+    result = celery.AsyncResult(task_id)
+    return result.status
